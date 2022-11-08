e94d7a4689cfb716eef5342d97c43990b84df3d9
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-11-09 00:18:23 +0530

    assignment4

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ header.style.border = '2px solid black';
 var addItem = document.getElementById('add-items');
 addItem.style.color = 'green';
 addItem.innerHTML = '<strong>Add Items</strong>'
+
+
+var items = document.getElementsByClassName('list-group-item');
+
+    items[2].style.backgroundColor = 'green';
+
+    for(var i = 0; i < items.length; i++)
+    {
+        items[i].style.fontWeight = 'bold';
+    }

5b2d48fea5850d30869b8bf484a6ddff0e25a40a
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-11-08 20:41:30 +0530

    AssignmentAdded

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("hellooo there!");
+var header = document.getElementById('header-title');
+
+header.style.border = '2px solid black';
+var addItem = document.getElementById('add-items');
+addItem.style.color = 'green';
+addItem.innerHTML = '<strong>Add Items</strong>'

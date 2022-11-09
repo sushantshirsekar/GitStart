5759bf6c4628301bb0475c605fc29428ef5235bd
sushantshirsekar <sushantshirsekar60@gmail.com>
2022-11-09 11:13:01 +0530

    assignment5

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ addItem.style.color = 'green';
 addItem.innerHTML = '<strong>Add Items</strong>'
 
 
-var items = document.getElementsByClassName('list-group-item');
+// var items = document.getElementsByClassName('list-group-item');
 
-    items[2].style.backgroundColor = 'green';
+//     items[2].style.backgroundColor = 'red';
 
-    for(var i = 0; i < items.length; i++)
-    {
-        items[i].style.fontWeight = 'bold';
-    }
+//     for(var i = 0; i < items.length; i++)
+//     {
+//         items[i].style.fontWeight = 'bold';
+//     }
+
+let x = document.getElementsByClassName('list-group-element');
+
+x[0].style.backgroundColor = 'red';
